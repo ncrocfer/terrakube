61928bdbeffcb5d91191f4d572ed55cd572d6a25
Nicolas Crocfer <nicolas.crocfer@ovhcloud.com>
2023-08-02 18:01:06 +0200

    fmt

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_keypair_v2" "sshkey-terrakube" {
-  provider   = "openstack.ovh"
+  provider   = openstack.ovh
   name       = "sshkey-terrakube"
   public_key = var.sshkey_terrakube
 }
